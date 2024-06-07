07204ec5b115e38e8ddfc1b81edbd6e8c1943af4
GitHub Action <action@github.com>
2024-06-07 03:00:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/cotrin8672/veriloghdlplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/cotrin8672/veriloghdlplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cotrin8672.veriloghdlplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
